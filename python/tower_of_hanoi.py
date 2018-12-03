5b13b62ae57b958494a1c5c87400545a38fbb00f
dipto0321 <diptokmk47@gmail.com>
2018-12-03 20:01:03 +0600

    Solve tower of hanoi problem

---
+++ python/tower_of_hanoi.py
@@ -0,0 +1,12 @@
+
+def tower(disk_numbers, source, auxilary, destination):
+    if disk_numbers == 1:
+        print(source + ' to ' + destination)
+        return
+    tower(disk_numbers - 1, source, destination, auxilary)
+    print(source + ' to ' + destination)
+    tower(disk_numbers - 1, auxilary, source, destination)
+    return
+
+
+# tower(6, '1', '2', '3')
---
+++ ruby/tower_of_hanoi.rb
@@ -0,0 +1,12 @@
+def tower(disk_numbers, source, auxilary, destination)
+  if disk_numbers == 1
+    puts "#{source} -> #{destination}"
+    return
+  end
+  tower(disk_numbers - 1, source, destination, auxilary)
+  puts "#{source} -> #{destination}"
+  tower(disk_numbers - 1, auxilary, source, destination)
+  nil
+end
+
+tower(2, '2', '1', '3')
