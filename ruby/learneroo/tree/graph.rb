fc031a55bc0b37aa3310feb50a92e32ad05a39c7
dipto0321 <diptokmk47@gmail.com>
2018-12-18 20:08:10 +0600

    Organise python file

--- ruby/learneroo/tree/graph.rb
+++ ruby/learneroo/tree/graph.rb
@@ -1,23 +1,36 @@
 # Graph implementation
+class Node
+  attr_accessor :value, :adjacents
+  def initialize(value)
+    @value = value
+    @adjacents = []
+  end
+end
 
+def arraytograph(arr)
+  nodes = []
+  0.upto(arr.size - 1) do |i|
+    node = Node.new(i)
+    arr[i].each { |val| node.adjacents.push(val) }
+    nodes.push(node)
+  end
+  nodes
+end
 
-=begin
+def visit(node, target, visited=[])
+  if node.data == target
+    return visited
+  else
+    if !visited.include?(node)
+      visited.push(node)
+      visit(node.adjacents[0], target, visited, node.adjacents)
+    end
+  end
+end
 
+def do_stuff(arr)
+  print visit(arraytograph(arr)[0], 4)
+end
+samples = [[2], [4], [5, 0, 3], [5, 2], [1, 5], [4, 2, 3]]
 
-Connections
-------------
-2
-4
-5 0 3
-5 2
-1 5
-4 2 3
-
-0: {2}
-1: {4}
-2: {5,0,3}
-3: {5,2}
-4: {1,5}
-5: {4,2,3}
-
-=end
+samples.each{ |el| do_stuff(el) }
