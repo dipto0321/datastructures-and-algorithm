cf41beb344e239dc7c6753d18462b7e296a3220b
dipto0321 <diptokmk47@gmail.com>
2019-01-15 00:09:00 +0600

    Make styles

--- ruby/learneroo/Tree And Graph Challenges/is_peak.rb
+++ ruby/learneroo/Tree And Graph Challenges/is_peak.rb
@@ -2,7 +2,7 @@ class Cell
   attr_accessor :data, :surroundings, :ruled_by
   def initialize(data)
     @data = data
-    @surroundings = {t: 0, r: 0, b: 0, l: 0}
+    @surroundings = { t: 0, r: 0, b: 0, l: 0 }
     @ruled_by = 0
   end
 
@@ -12,7 +12,6 @@ class Cell
     print "Surroundings: #{@surroundings}\n"
     puts  "Ruled_by: #{@ruled_by}"
     puts ''
-
   end
 end
 
@@ -27,7 +26,7 @@ class GridsInfo
   def show
     puts ''
     print "Peaks: #{@peaks}"
-    @cell_infos.each { |cell_info| cell_info.show }
+    @cell_infos.each(&:show)
     print "Areas: #{@areas}"
     puts ''
   end
@@ -41,11 +40,11 @@ def generate_grid_info(grid)
   grid_info = GridsInfo.new
 
   grid.each_with_index do |items, row|
-    items.each_with_index do |item, col|
+    items.each_with_index do |_item, col|
       cell = Cell.new(grid[row][col])
       cell.surroundings[:t] = row - 1 < 0 ? 0 : grid[row - 1][col]
       cell.surroundings[:r] = col + 1 > grid.size - 1 ? 0 : grid[row][col + 1]
-      cell.surroundings[:b] = row + 1 > items.size - 1 ? 0 : grid[row + 1 ][col]
+      cell.surroundings[:b] = row + 1 > items.size - 1 ? 0 : grid[row + 1][col]
       cell.surroundings[:l] = col - 1 < 0 ? 0 : grid[row][col - 1]
       cell.ruled_by = ruling_party(cell.data, cell.surroundings.values.max)
       grid_info.cell_infos.push(cell)
@@ -61,18 +60,18 @@ def do_stuff(grid)
   grid_information.cell_infos.each do |cell|
     grid_information.areas.keys.each do |key|
       updated = false
-      if (cell.ruled_by.zero? && grid_information.areas[key].empty?) ||(!grid_information.areas[key].empty? && grid_information.areas[key].include?(cell.ruled_by)) 
+      if (cell.ruled_by.zero? && grid_information.areas[key].empty?) || (!grid_information.areas[key].empty? && grid_information.areas[key].include?(cell.ruled_by))
         grid_information.areas[key].push(cell.data)
         updated = true
       end
       break if updated
     end
   end
-  res = grid_information.areas.values.map { |el| el.count }
+  res = grid_information.areas.values.map(&:count)
   # puts [res.min, res.max].join(' ')
   grid_information.show
 end
 
-inputs = [[9,8,5],[5,6,3],[8,4,1]]
+inputs = [[9, 8, 5], [5, 6, 3], [8, 4, 1]]
 
 do_stuff(inputs)
--- ruby/learneroo/Tree And Graph Challenges/is_peak_rec.rb
+++ ruby/learneroo/Tree And Graph Challenges/is_peak_rec.rb
@@ -25,7 +25,7 @@ def create_graph(grid)
 
   cell_grid.each_with_index do |cell_row, row|
     cell_row.each_with_index do |cell, col|
-      row == 0 ? nil : cell.surroundings.push(cell_grid[row-1][col])
+      row == 0 ? nil : cell.surroundings.push(cell_grid[row - 1][col])
       col == cell_row.size - 1 ? nil : cell.surroundings.push(cell_grid[row][col + 1])
       col == 0 ? nil : cell.surroundings.push(cell_grid[row][col - 1])
       row == cell_grid.size - 1 ? nil : cell.surroundings.push(cell_grid[row + 1][col])
@@ -39,25 +39,27 @@ def find_peaks(cell_grid)
   peaks = []
   cell_grid.each do |cell_row|
     cell_row.each do |cell|
-      peaks.push(cell) if cell.surroundings.all? {|neighbor| cell.data >= neighbor.data}
+      peaks.push(cell) if cell.surroundings.all? { |neighbor| cell.data >= neighbor.data }
     end
   end
   peaks
 end
-# 2. Method to determine which peak a cell belongs to
-  def set_belongs_to(peaks_arr, cell)
-    return cell if peaks_arr.include?(cell)
-    
-    max_neighbor_data = cell.surroundings.map {|neighbor| neighbor.data}.max
 
-    max_neighbor = nil
-    cell.surroundings.each do |cell|
-      max_neighbor = cell if cell.data == max_neighbor_data
-    end
-    
-    return cell if max_neighbor_data <= cell.data
-    set_belongs_to(peaks_arr, max_neighbor)
+# 2. Method to determine which peak a cell belongs to
+def set_belongs_to(peaks_arr, cell)
+  return cell if peaks_arr.include?(cell)
+
+  max_neighbor_data = cell.surroundings.map(&:data).max
+
+  max_neighbor = nil
+  cell.surroundings.each do |cell|
+    max_neighbor = cell if cell.data == max_neighbor_data
   end
+
+  return cell if max_neighbor_data <= cell.data
+
+  set_belongs_to(peaks_arr, max_neighbor)
+end
 
 # 3. Method to count cells controlled by each peak
 
@@ -74,16 +76,15 @@ end
 def count_cells(grid_map)
   areas_hash = {}
   grid_map.each do |arr|
-    arr.each {|peak| areas_hash[peak].nil? ? areas_hash[peak] = 1 : areas_hash[peak]+=1}
+    arr.each { |peak| areas_hash[peak].nil? ? areas_hash[peak] = 1 : areas_hash[peak] += 1 }
   end
-  [areas_hash.values.min, areas_hash.values.max].join(" ")
+  areas_hash.values.minmax.join(' ')
 end
 
 def do_stuff(grid)
   puts count_cells(map_area(grid))
 end
 
-
-inputs = [[9,8,5],[5,6,3],[8,4,1]]
+inputs = [[9, 8, 5], [5, 6, 3], [8, 4, 1]]
 
 do_stuff(inputs)
