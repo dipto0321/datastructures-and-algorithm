b15b804b0db82a80ffe6131d5d0c6f3ecd532698
dipto0321 <diptokmk47@gmail.com>
2019-01-31 08:08:08 +0600

    Apply rubocop

--- ruby/learneroo/Shorting_Algorithms/prep_for_full_sort_cmd_input.rb
+++ ruby/learneroo/Shorting_Algorithms/prep_for_full_sort_cmd_input.rb
@@ -9,9 +9,9 @@ def do_stuff(ar)
 end
 
 t = gets.to_i
-for i in 1..t do
-		n=gets
-    ar = gets.strip.split.map{|i| i.to_i if i.scan(/^\d+$/).any? }
-    ar.delete(nil)
-    do_stuff(ar)
+(1..t).each do |_i|
+  n = gets
+  ar = gets.strip.split.map { |i| i.to_i if i.scan(/^\d+$/).any? }
+  ar.delete(nil)
+  do_stuff(ar)
 end
