9dc10e2130fd6a73c4c06e8bf5af1a2ebbc169e1
dipto0321 <diptokmk47@gmail.com>
2019-02-14 07:58:32 +0600

    Solve index match problem with binary search
    It will be solve by simple linear search which took O(n) time. But in
    binary search it took O(log n) time.

--- ruby/learneroo/Sorting_Challeneges/index_match_w_merge.rb
+++ ruby/learneroo/Sorting_Challeneges/index_match_w_merge.rb
@@ -1,8 +1,18 @@
-def index_match(ar, mid)
-  
+def binary_search(arr, low, high)
+  if high >= low
+    mid = (low + high) / 2
+    return mid if mid == arr[mid]
+    if mid > arr[mid]
+      return binary_search(arr, mid + 1, high)
+    else
+      return binary_search(arr, low, mid - 1)
+    end
+  end
+  -1
 end
 
-def do_stuff(ar, mid)
-  
+def do_stuff(ar)
+  puts binary_search(ar, 0, ar.size - 1)
 end
+
 do_stuff([3, 5, 7, 9, 11, 13, 17])
