e31600a99f769d5753f09420ec7bbe29d7113a94
dipto0321 <diptokmk47@gmail.com>
2019-04-02 17:50:34 +0600

    Solve day 1 problem

--- python/Daily_Coding_Problem/problem_1.py
+++ python/Daily_Coding_Problem/problem_1.py
@@ -5,4 +5,16 @@
 # For example, given[10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 arr = [10, 15, 3, 7]
-k = 18
+k = 20
+
+
+def check_sum(numbers, target):
+    num_pairs = set()
+    for num in numbers:
+        if target - num in num_pairs:
+            return True
+        else:
+            num_pairs.add(num)
+
+
+print(check_sum(arr, k))
