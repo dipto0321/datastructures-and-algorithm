8e6b73c254799f4397741d4a8b616655e8f148c2
Dipto Karmakar <diptokmk47@gmail.com>
2019-04-20 21:12:16 +0600

    Daily Coding Problem: Problem #2 [Hard]
    - Solves by python. Use numpy for product a list items

---
+++ python/Daily_Coding_Problem/problem_2.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def new_array(exclud, el):
+    if el is not exclud:
+        return el
+
+
+def multy_array(_items):
+    res = []
+    for i in range(0, len(_items)):
+        list_copy = _items.copy()
+        list_copy.pop(i)
+        res.append(np.prod(list_copy))
+    return res
+
+
+inputs = [[1, 2, 3, 4, 5], [3, 2, 1]]
+
+for _input in inputs:
+    print(multy_array(_input))
