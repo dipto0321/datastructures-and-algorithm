45405bd984cfd0c7f8e676dc362e8d27a70ce6f8
Dipto Karmakar <diptokmk47@gmail.com>
2019-04-22 08:34:04 +0600

    Add two_sum another solution using hash table
    - Runtime: 36 ms, faster than 100.00% of Ruby online submissions for Two
    Sum.
    - Memory Usage: 10 MB, less than 51.55% of Ruby online submissions for
    Two Sum.

--- ruby/Leetcode/two_sum.rb
+++ ruby/Leetcode/two_sum.rb
@@ -1,12 +1,23 @@
-def two_sum(arr, target)
-  (0...arr.size).each do |_i|
-    (_i+1...arr.size).each do |_j|
-      return [_i, _j] if arr[_i] + arr[_j] == target
-    end
+# Brute force solution
+
+# def two_sum(arr, target)
+#   (0...arr.size).each do |_i|
+#     (_i+1...arr.size).each do |_j|
+#       return [_i, _j] if arr[_i] + arr[_j] == target
+#     end
+#   end
+# end
+
+def two_sum(nums, target)
+  nums_hash = {}
+  nums.each_with_index do |val, i|
+    complement = target - nums[i]
+    return [i, nums_hash[complement]] if nums_hash.key?(complement)
+    nums_hash[val] = i
   end
 end
 
-inputs = [3,3]
-target = 6
+inputs = [2, 7, 11, 15]
+target = 9
 
 print two_sum(inputs, target)
