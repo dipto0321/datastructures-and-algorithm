e4c41ec19a4b5501323504c7e23a5bee4e313155
Dipto Karmakar <diptokmk47@gmail.com>
2019-09-27 07:41:45 +0600

    Add solution for max_profit 2

--- python/leetcode/make_profit_II.py
+++ python/leetcode/make_profit_II.py
@@ -3,4 +3,15 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        pass
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i - 1]
+        return profit
+
+
+prices: List[int] = [7, 1, 5, 3, 6, 4]
+
+sol = Solution()
+
+print(sol.maxProfit(prices))
